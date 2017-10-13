2865c46e8790185100bcec39e9977553ddbb238d
Unknown <plop-113@hotmail.com>
2017-10-13 14:32:32 +0200

    Ejercicio 7 terminado

---
+++ 07_valor_absoluto/Ejercicio7/src/Ejercicio7.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ejercicio7 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Escriba un numero y le diremos su valor absoluto");
+        int a;
+        do {
+            a = Integer.parseInt(br.readLine());
+
+
+            if (a > 0) {
+                System.out.println("El valor absoluto de " + a + " es: " + a);
+
+            } else {
+                System.out.println("El valor absoluto de " + a + " es: " + a * -1);
+            }
+
+        } while (a != 0);
+        System.out.println("Programa terminado. ");
+    }
+}
+
+
+
+
