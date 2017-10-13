93c1e6dacb84a03a88c2b4068a89102bb94e73d5
Unknown <plop-113@hotmail.com>
2017-10-13 14:32:39 +0200

    Ejercicio 8 terminado

---
+++ 08_suma_10/Ejercicio8/src/Ejercicio8.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ejercicio8 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader (System.in));
+        System.out.println("Escriba 10 numeros y los sumaremos.");
+    int suma=0;
+
+        for(int i=0;i<10;i++){
+        int n=Integer.parseInt(br.readLine());
+        suma=suma+n;
+    }
+        System.out.println("La suma de todos los numeros es: "+suma);
+}
+}
