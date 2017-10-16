4067bbb932c49db67bc2c285182abcfe1a13b8e0
Unknown <plop-113@hotmail.com>
2017-10-16 21:00:13 +0200

    Ejercicio 10 terminado

---
+++ 10_temperatura/Ejercicio10/src/Ejercicio10.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ejercicio10 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Introduzca la temperatura en Cº ");
+        double c=Double.parseDouble(br.readLine());
+        System.out.println("Escriba la temperatura deseada a convertir");
+
+        String temperatura="T";
+        temperatura=br.readLine();
+        switch(temperatura.toUpperCase()){
+            case "F":
+                c=(1.8*c)+32;
+                System.out.println("La temperatura en Fahrenheit es: "+c+" F");break;
+            case "f":
+                c=(1.8*c)+32;
+                System.out.println("La temperatura en Fahrenheit es: "+c+" F");break;
+            case"K":
+                c=c+273.15;
+                System.out.println("La temperatura en Kelvin es: "+c+" K ");break;
+            case"k":
+                c=c+273.15;
+                System.out.println("La temperatura en Kelvin es: "+c+" K ");break;
+
+        }
+
+    }
+
+    }
+
