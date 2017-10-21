d86bc9f232b7ce28bd14b1649dcaae32895596fd
Unknown <plop-113@hotmail.com>
2017-10-21 15:32:55 +0200

    Ejercicio 9 corregido

--- 09_suma_n/Ejercicio9/src/Ejercicio9.java
+++ 09_suma_n/Ejercicio9/src/Ejercicio9.java
@@ -12,6 +12,7 @@ public class Ejercicio9 {
         int suma=0;
         for(int i=0;i<c;i++){
             int n=Integer.parseInt(br.readLine());
+            System.out.println("Escribe otro número.");
             suma=suma+n;
         }
         System.out.println("La suma de todos los numeros es: "+suma);
